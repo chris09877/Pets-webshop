67cd29296742bd7d40eb22d187f23d7f7dd8ba52
chris09877 <104131775+chris09877@users.noreply.github.com>
2024-02-28 15:45:14 +0100

    first commit

---
+++ dierenwinkel/src/main/java/chris/ilg/dierenwinkel/DierenwinkelApplication.java
@@ -0,0 +1,13 @@
+package chris.ilg.dierenwinkel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DierenwinkelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DierenwinkelApplication.class, args);
+    }
+
+}
