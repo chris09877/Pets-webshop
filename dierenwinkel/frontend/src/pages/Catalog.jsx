651232ee8b4249425eca8996f91c34a15206f6da
chris09877 <104131775+chris09877@users.noreply.github.com>
2024-03-21 12:25:21 +0100

    tailwin config ok

--- dierenwinkel/frontend/src/pages/Catalog.jsx
+++ dierenwinkel/frontend/src/pages/Catalog.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Catalog = () => {
     const [products, setProducts] = useState([]);
+    const [showBtn, setShowBtn] = useState(true);
+
     useEffect(() => {
         async function getCsrfToken() {
             let cookieArray = document.cookie.split(';');
@@ -21,7 +24,7 @@ const Catalog = () => {
     
         async function fetchProducts() {
             const token = await getCsrfToken();
-            console.log(token); // Log the CSRF token
+            console.log(token); 
             
             try {
                 const response = await axios.get('http://localhost:8080/api/product/all', {
@@ -29,65 +32,111 @@ const Catalog = () => {
                     //     'X-CSRF-Token': token
                     // },
                 });
-                return response.data; // Return the fetched products data
+                return response.data; 
             } catch (error) {
                 console.error('Error fetching products:', error);
-                return []; // Return an empty array in case of error
+                return []; 
             }
         }
     
         async function loadProducts() {
             const products = await fetchProducts();
-            console.log(products); // Log the fetched products
-            setProducts(products); // Update the products state
+            console.log(products); 
+            setProducts(products); 
         }
     
         loadProducts();
-    }, []); // Empty dependency array to run the effect only once
-    
-    
-    // async function getCsrfToken(){
-    //     let cookieArray = document.cookie.split(';');
-    //     let csrfToken = null;
-    
-    //     cookieArray.forEach(cookie => {
-    //         if (cookie.startsWith("_xsrf=")) {
-    //             csrfToken = cookie.substring("_xsrf=".length, cookie.length);
-    //         }
-    //     });
-    
-    //     return csrfToken;
-    // }
-    
 
-    // async function getProducts() {
+
         
-    //     const token = await getCsrfToken();
-    //     console.log(token);
-    //     try {
-    //         const response = await axios.get('http://localhost:8080/api/product/all',
-    //         {
-    //             headers: {
-    //               'X-CSRF-Token': token
-    //             },
-    //           }
-    //         );
-    //         let data = response.data;
-    //         console.log(data);
-    //     }
-    //     catch (error) {
-    //         console.error('Error creating user:', error);
-    //     }
-    // };
+    }, []); 
+    
+    const handleOrderClick = async () => {
+        
+    };
 
-    // getProducts();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // // Collect form data
+        // const formData = new FormData(e.target);
+        // const data = {};
+        // formData.forEach((value, key) => {
+        //     data[key] = value;
+        // });
+        // data['user'] = localStorage.getItem('userId');
+        // console.log(`data in handlesubmit: ${data}`);
+        // try {
+        //     console.log(`${config.apiUrl}/orders/update/${localStorage.getItem('userId')}`);
+        //     const response = await axios.patch(`${config.apiUrl}/orders/update/${localStorage.getItem('userId')}`, data);
+
+        //     //console.log(response.data); // Handle the response here (success message or other actions)
+
+        //     //   setOrderDetails(await response.data);
+        //     //   console.log(` data send to component: ${orderDetails}`);
 
 
+        // } catch (error) {
+        //     console.error('Error:', error); // Handle errors here
+        // }
+    };
 
     return (
         <>
-        <h1>product</h1>
-        <p>{products}</p>
+        {/* <h1>product</h1>
+        <ul>
+                {products.map(product => (
+                    <a href={`http://localhost:5173/${product.id}`}>
+                    <li key={product.id}>
+                        <h2>{product.name}</h2>
+                        <p>Description: {product.description}</p>
+                        <p>Price: {product.price}</p>
+                        <p>Quantity: {product.quantity}</p>
+                    </li>
+                    </a>
+                ))}
+            </ul> */}
+
+
+            <h1>MENU</h1>
+            {Array.isArray(products) &&
+
+                <div className="flex justify-center items-center">
+                    {products.map((product) => (
+                        <form key={product.id} onSubmit={(e) => handleSubmit(e)}>
+                            <div className="bg-white p-4 shadow-md">
+                                <Link to={`/products/${product.id}`} className="block">
+                                    <h2 className="text-lg font-semibold mb-2">{product.name}</h2>
+                                    <img src={`/${product.name}.jpg`} alt={product.name} className="w-full h-40 object-cover mb-2" />
+                                    <p className="text-gray-600 mb-2">{product.price}</p>
+                                    <p className="text-sm text-gray-500 mb-2">{product.description}</p>
+
+                                </Link>
+                                <label className="block mb-2">
+                                    Quantity:
+                                    <input
+                                        type="number"
+                                        name="quantity"
+                                        defaultValue={1}
+                                        min={1}
+                                        className="block w-full border border-gray-300 rounded px-2 py-1"
+                                    />
+                                </label>
+                                <button hidden={showBtn} type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                                    Add
+                                </button>
+                                <input type="hidden" name="name" value={product.name} />
+                                <input type="hidden" name="price" value={product.price} />
+                                <input type="hidden" name="user" value={""} />
+                                <input type="hidden" name="product_id" value={product.id} />
+                            </div>
+                        </form>
+                    ))}
+                </div>
+
+            }            
+
+
         </>
     );
 
