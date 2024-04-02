e3fe420ee2831727b6a8840a5024289340b36255
chris09877 <104131775+chris09877@users.noreply.github.com>
2024-04-02 18:02:38 +0200

    style home page done

--- dierenwinkel/frontend/src/pages/Home.jsx
+++ dierenwinkel/frontend/src/pages/Home.jsx
@@ -49,7 +49,7 @@ const Home = () => {
       <header className="bg-custom-gray text-white p-4 flex items-center justify-between">
         <h1 className="text-2xl font-bold">Pets Pals</h1>
         <div className="flex-grow flex justify-center">
-          <img src="path/to/your/logo.png" alt="Logo" className="w-20 h-20" /> {/* Adjust the w-20 h-20 to fit your logo size */}
+          <img src="path/to/your/logo.png" alt="Logo" className="w-20 h-20" /> {/* Adjust the w-20 h-20 to fit logo size */}
         </div>
         <div className="flex justify-end">
           <NavBar />
@@ -67,32 +67,49 @@ const Home = () => {
 
         <div className=" text-white p-8">
           <div className=" text-white p-8">
-            <h2 className="text-3xl mb-4 cursor-pointer border-t border-b border-white py-4 px-4" onClick={toggleHowItWorks}>
-              How PetPals Works
+            <h2 className="text-3xl mb-4 cursor-pointer border-t border-b border-white py-4 px-4  flex justify-between" onClick={toggleHowItWorks}>
+              How PetPals Works {isHowItWorksExpanded ? <span className="mr-4"> &uarr; </span> : <span > &darr; </span>}
             </h2>
             {isHowItWorksExpanded && (
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12">
-                {/* Step 1 */}
-                {/* ... */}
-                {/* Step 2 */}
-                {/* ... */}
-                {/* Step 3 */}
-                {/* ... */}
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              <div>
+                <img src="/howPetPals1.png" alt="Pet Sitter" className="w-full" />
+                <h3 className="text-xl font-bold mt-2">Step 1:Find your product</h3>
+                <p className="mt-1">Our shop has a wide range of different kinds of products for almost 100 different species.</p>
               </div>
+
+              <div>
+                <img src="/howPetPals2.png" alt="Supplier" className="w-full" />
+                <h3 className="text-xl font-bold mt-2">Step 2:Add to cart</h3>
+                <p className="mt-1">Browse through our selection of pets and products to find your perfect match.</p>
+              </div>
+
+              <div>
+                <img src="/howPetPals3.png" alt="Team Member" className="w-full" />
+                <h3 className="text-xl font-bold mt-2">Step 3:Confirm your order</h3>
+                <p className="mt-1">Complete your payement and relax while we prepare your otder for delivery=</p>
+              </div>
+            </div>
             )}
 
-            <h2 className="text-3xl mb-4 cursor-pointer border-t border-b border-white py-4 px-4" onClick={toggleJoinCommunity}>
-              Join the PetPals Community
+            <h2 className="text-3xl mb-4 cursor-pointer border-t border-b border-white py-4 px-4 flex justify-between" onClick={toggleJoinCommunity}>
+              Join the PetPals Community {isJoinCommunityExpanded ? <span className="mr-4"> &uarr; </span> : <span > &darr; </span>}
             </h2>
             {isJoinCommunityExpanded && (
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                {/* As a Pet Sitter */}
-                {/* ... */}
-                {/* As a Supplier */}
-                {/* ... */}
-                {/* As a Team Member */}
-                {/* ... */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div>
+                <img src="/petFriend.png" alt="Pet Sitter" className="w-full" />
+                <h3 className="text-xl font-bold mt-2"> As a pet friend</h3>
+                <p className="mt-1">Share or learn from other pet friends how they take care of their pets.</p>
               </div>
+
+              <div>
+                <img src="/supplier.png" alt="Supplier" className="w-full" />
+                <h3 className="text-xl font-bold mt-2">As a supplier</h3>
+                <p className="mt-1">Provide quality supplies for pets.</p>
+              </div>
+
+            </div>
             )}
           </div>
         </div>
